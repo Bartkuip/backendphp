e9d50d40014b144c279e12a3ea1efc7ba10e8332
Bartkuip <99027534@mydavinci.nl>
2023-05-23 15:30:48 +0200

    deel 3 begonnen

--- formulier/madlibs/paniekResult.php
+++ formulier/madlibs/paniekResult.php
@@ -16,21 +16,10 @@
     <p> Er heerst paniek in het koningkrijk <?php echo $_POST["land"]; ?>, Koning <?php echo $_POST["spijbel"]; ?> is ten einde raad en als koning <?php echo $_POST["spijbel"]; ?> ten einde raad is dan roept hij zijn ten-einde-raadheer <?php echo $_POST["persoon"]; ?></p>
     <p>"<?php echo $_POST["persoon"]; ?>! Het is ramp! Het is een schande!" </p>
     <p>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?" </p>
-    <p>"Mijn schildpad is verdwenen! Zo maar, zonder waarschuwing. En ik had net Lego voor hem gekocht!" </p>
-    <p>Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd programmeren leren?"" </p>
-    <p>Maar Sire, daar kunt u toch uw supercomputer voor gebruiken.</p>
-    <p>Spinoza, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had. </p>
-    <p>"Mij vervelen, Sire" </p>
-    <?php
-        echo "<h2>Your Input:</h2>";
-        echo $_POST["huisdier"];
-        echo $_POST["persoon"];
-        echo $_POST["land"];
-        echo $_POST["verveel"];
-        echo $_POST["kind"];
-        echo $_POST["spijbel"];
-        echo $_POST["euro"];
-        echo $_POST["bezig"];
-    ?>
+    <p>"Mijn <?php echo $_POST["huisdier"]; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["kind"]; ?> voor hem gekocht!" </p>
+    <p>Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST["bezig"]; ?> leren?"" </p>
+    <p>Maar Sire, daar kunt u toch uw <?php echo $_POST["euro"]; ?> voor gebruiken.</p>
+    <p><?php echo $_POST["persoon"]; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had. </p>
+    <p>"<?php echo $_POST["verveel"]; ?>, Sire" </p>
 </body>
 </html>
